e5e582ba0b7555815d7d66fea47d157097a3919a
Drunken Unicorn <1425653+drunkenunicorn1972@users.noreply.github.com>
2025-10-12 19:39:18 +0200

    working on warnings caused by deprecated notations

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -11,6 +11,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminDashboard;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
